f3820c5963b68081a2e55d0ebe36688e29869197
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2019-10-18 09:28:31 -0700

    Map code done successfully

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.HashMap;
+public class Main {
+    public static void main(String[] args) {
+        Map<String,String> map=new HashMap<>();
+        map.put("my name is","divyanshu");
+        map.put("my father name is","rajeev sharma");
+        map.put("my mother name is","indresh sharma");
+        System.out.println(map);
+    }
+}
