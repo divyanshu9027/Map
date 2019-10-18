e89fb2dfe9fa1bd32b6ab5a73c5f5f6e5b3e9d8d
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2019-10-18 09:39:06 -0700

    Map code done successfully and appers

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Map<String,String> map=new HashMap<>();
         map.put("my name is","divyanshu");
         map.put("my father name is","rajeev sharma");
         map.put("my mother name is","indresh sharma");
-        System.out.println(map);
+        Set<String> keys=map.keySet();
+        for(String key: keys)
+        {
+            System.out.println(key+ " "+map.get(key));
+
+        }
+
+
     }
 }
